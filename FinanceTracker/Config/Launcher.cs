fb16d714efa725037837592bd642409935d8f067
Kodytek Jakub <jkodytek@retia.cz>
2024-02-22 13:54:13 +0100

    Přidejte soubory projektu.

---
+++ FinanceTracker/Config/Launcher.cs
@@ -0,0 +1,28 @@
+﻿using FinanceTracker.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceTracker.Config
+{
+    public class Launcher
+    {
+        private DatabaseConnector DatabaseConnector { get; set; }
+        private GraphTest GraphTest { get; set; }
+        private MainWindow MainWindow { get; set; }
+
+        public Launcher(MainWindow mainWindow)
+        {
+            MainWindow = mainWindow;
+            DatabaseConnector = new();
+            GraphTest = new(MainWindow);
+        }
+
+        public void Launch()
+        {
+            Console.WriteLine("FinanceTracker is running...");
+        }
+    }
+}
